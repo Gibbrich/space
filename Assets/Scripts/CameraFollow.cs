59eecdbba13539f7004139c3b5be170d1143c547
Vasiliy M <vasmark.karjala@gmail.com>
2019-11-17 14:20:12 +0300

    Add CameraFollow.cs and new sprite

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private float posX;
+    private float posY;
+
+    void FixedUpdate()
+    {
+        Follow();
+    }
+
+    private void Follow()
+    {
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(player);
+    }
+}
