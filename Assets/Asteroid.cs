f2575f017868f9a17712276405ddd180f0359f9c
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2019-11-17 11:51:20 +0300

    Add scoring

--- Assets/Asteroid.cs
+++ Assets/Asteroid.cs
@@ -5,11 +5,28 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
+    public int Score = 10;
+    
+    private GameController gameController;
+
+    private void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (!other.gameObject.CompareTag("Asteroid"))
         {
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            gameController.UpdateScore(Score);
         }
     }
 }
