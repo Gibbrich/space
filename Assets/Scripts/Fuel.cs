5d869f8e79fd532036320aab3164a0ce484a539f
Arthur Dvurechenskiy <gibbrich@yandex.ru>
2019-11-17 22:29:58 +0300

    Fuel animation fix

--- Assets/Scripts/Fuel.cs
+++ Assets/Scripts/Fuel.cs
@@ -1,23 +1,34 @@
 
 using System;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class Fuel: MonoBehaviour
 {
     public float Value = 30;
     public float Lifetime = 9;
+    [Range(0, 1)] public float StartAnimationThreshold;
 
     private GameController gameController;
     private float createTime;
+    private Animator animator;
+    private bool isAnimationStarted;
 
     private void Start()
     {
         gameController = FindObjectOfType<GameController>();
         createTime = Time.time;
+        animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
+        if (!isAnimationStarted &&Time.time - createTime >= Lifetime * (1 - StartAnimationThreshold))
+        {
+            animator.SetTrigger("Start");
+            isAnimationStarted = true;
+        }
+        
         if (Time.time - createTime >= Lifetime)
         {
             Destroy(gameObject);
